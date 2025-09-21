d047afe9ca923f1b4ad4a58b8fac4adc84286078
sssfind <110827622+sssfind@users.noreply.github.com>
2025-09-21 18:03:04 +0500

    Update ClueTest.java

--- src/ClueTest.java
+++ src/ClueTest.java
@@ -8,7 +8,7 @@ class ClueTest {
     void getclue() {
         Clue clue = new Clue("Москва");
         assertAll(() -> assertEquals("Первая буква: М", clue.getclue()),
-                () -> assertEquals("Вторая буква: ", clue.getclue()),
-                () -> assertEquals("Третья буква: Мо", clue.getclue()));
+                () -> assertEquals("Вторая буква: Мо", clue.getclue()),
+                () -> assertEquals("Третья буква: Мос", clue.getclue()));
     }
 }
